58af2dc304c65a242d5684bf7d85a859eebe610e
sarunh10 <sarunh10@gmail.com>
2025-12-06 10:25:05 +0000

    task 0

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -2,21 +2,24 @@
 import sys
 
 def fizzbuzz(n):
+    result = []
     for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
+        if i % 15 == 0:
+            result.append("FizzBuzz")
         elif i % 3 == 0:
-            print("Fizz", end=" ")
+            result.append("Fizz")
         elif i % 5 == 0:
-            print("Buzz", end=" ")
+            result.append("Buzz")
         else:
-            print(i, end=" ")
-    print()
+            result.append(str(i))
+    print(" ".join(result))
+
 
 if __name__ == "__main__":
     try:
         n = int(sys.argv[1]) if len(sys.argv) > 1 else 100
     except ValueError:
         n = 100
+
     fizzbuzz(n)
-0   
+  
